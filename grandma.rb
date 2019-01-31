db410cd4e1394f4fb41c242e0300911a7520afc7
Jessy Hu <jesshuy12@gmail.com>
2019-01-31 22:01:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,3 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase.upcase != phrase
     return "HUH?! SPEAK UP, SONNY! "
+  elsif phrase === "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return "NO, NOT SINCE 1938!"
+  end
+end
